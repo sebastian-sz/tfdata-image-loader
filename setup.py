2019c8d3df7e27e96151ed8dbc62022e0c952922
sebastian-sz <mocart15@gmail.com>
2021-03-19 11:50:55 +0100

    Added Lint job to github actions.

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name="tfdata_image_loader",
     version="0.1",
     packages=find_packages(),
-    description="A custom module for loading image data for Image Classification problem.",
+    description="A custom module for loading image data "
+                "for Image Classification problem.",
     author="Sebastian Sz",
 )
