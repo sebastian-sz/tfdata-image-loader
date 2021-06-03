2fe6d18c1211bc8ac1a49a143a08a9851797b5a7
sebastian-sz <mocart15@gmail.com>
2021-06-03 17:25:23 +0200

    Refactored repo to be a Python package.

---
+++ tests/_root_dir.py
@@ -0,0 +1,4 @@
+"""Module for handling data paths."""
+import os
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
---
+++ tfdata_image_loader/__init__.py
@@ -0,0 +1,5 @@
+"""Package for importing TFDataImageLoader."""
+
+from tfdata_image_loader.tfdata_image_loader import TFDataImageLoader
+
+__all__ = ["TFDataImageLoader"]
